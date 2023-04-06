000b1a2a173d04ca6295b962c35da6aba4281010
Saireddy9966 <krishnareddyv69@gmail.com>
2023-04-06 21:50:09 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from app.models import *
+from django.
 
 # Create your views here.
 def display_topics(request):
@@ -9,5 +10,15 @@ def display_topics(request):
 
 def display_webp(request):
     wp=Webpage.objects.all()
+    wp=Webpage.objects.order_by('name')
+    wp=Webpage.objects.order_by('-name')
+    wp=Webpage.objects.all()[:2:]
+    wp=Webpage.objects.filter(topic_name='cricket')
+    wp=Webpage.objects.order_by('-name')
     d={'web':wp}
     return render(request,'display_webp.html',context=d)
+
+def display_access(request):
+    ar=Accessrecord.objects.all()
+    d={'accessr':ar}
+    return render(request,'display_access.html',context=d)
--- project30/urls.py
+++ project30/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('display_topics/',display_topics,name='display_topics'),
     path('display_webp/',display_webp,name='display_webp'),
+    path('display_access/',display_access,name='display_access'),
+
 
 ]
